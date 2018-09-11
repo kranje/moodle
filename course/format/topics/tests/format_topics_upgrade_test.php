8f89f26e1c358d9ac367dbfb198108d4d17b2d8b
Charles Fulton <fultonc@lafayette.edu>
2018-09-11 10:51:09 -0400

    Revert "MDL-62534 format_topics: do not purge empty sections on upgrade"
    This reverts commit 5abeb8048d538e241a918fdef5be49eb941668af.

--- course/format/topics/tests/format_topics_upgrade_test.php
+++ course/format/topics/tests/format_topics_upgrade_test.php
@@ -125,28 +125,4 @@ class format_topics_upgrade_testcase extends advanced_testcase {
         // The module is still visible.
         $this->assertEquals(1, $DB->get_field('course_modules', 'visible', ['id' => $cm->cmid]));
     }
-
-    /**
-     * Test upgrade step to add empty sections.
-     */
-    public function test_numsections_add_empty_sections() {
-        global $DB;
-
-        $this->resetAfterTest(true);
-
-        $params = array('format' => 'topics', 'numsections' => 16, 'startdate' => 1445644800);
-        $course = $this->getDataGenerator()->create_course($params);
-
-        // This test is executed after 'numsections' option was already removed.
-        $DB->insert_record('course_format_options', ['courseid' => $course->id, 'format' => 'topics',
-            'sectionid' => 0, 'name' => 'numsections', 'value' => '16']);
-
-        // There are 16 sections.
-        $this->assertEquals(17, $DB->count_records('course_sections', ['course' => $course->id]));
-
-        format_topics_upgrade_remove_numsections();
-
-        // There should still be 16 sections.
-        $this->assertEquals(17, $DB->count_records('course_sections', ['course' => $course->id]));
-    }
 }
