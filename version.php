2ae04e9ffecab9d00df81a08075a3e679872dc76
Charles Fulton <fultonc@lafayette.edu>
2019-07-09 10:02:25 -0400

    Squashed 'blocks/clampmail/' changes from 4d6c22fb07f..c1c9e503b21
    c1c9e503b21 CLAMPMail v3.5.0
    d346de28ff2 46 internal navigation (#47)
    4dbd6fbf48e Update Java package in Travis
    895320e4e98 CLAMPMail v3.3.7
    85c49b723f0 Allow adding to activity modules; fixes #44 (#45)
    8b64a76380e CLAMPMail v3.3.6
    97a7911ecae Merge pull request #43 from CLAMP-IT/41-jquery
    e8dfb70a5cd Remove no-op
    db62a487b7b Refactor jQuery form handling; fixes #41
    2d0d9828292 Exclude PHP 7.2 on Moodle 3.3
    2015a97e21f Add PHP 7.2
    
    git-subtree-dir: blocks/clampmail
    git-subtree-split: c1c9e503b21ab1dd9f954683fa8f39253da021b3

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019010800; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017051500; // Requires this Moodle version.
+$plugin->version   = 2019070900; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018051700; // Requires this Moodle version.
 $plugin->component = 'block_clampmail'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE; // The current stability of this version (used for diagnostics).
-$plugin->release   = 'v3.3.5'; // The release name of this version (used for diagnostics).
+$plugin->release   = 'v3.5.0'; // The release name of this version (used for diagnostics).
