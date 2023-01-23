983a0fb03fddc8624a9c4199554e031b2d97d19d
Charles Fulton <fultonc@lafayette.edu>
2023-01-23 14:43:57 -0500

    Update test

--- reportbuilder/tests/local/aggregation/sum_test.php
+++ reportbuilder/tests/local/aggregation/sum_test.php
@@ -120,6 +120,10 @@ class sum_test extends core_reportbuilder_testcase {
                 'c1_suspended' => '0 suspended',
             ],
             [
+                'c0_firstname' => 'Anonymous',
+                'c1_suspended' => 0,
+            ],
+            [
                 'c0_firstname' => 'Bob',
                 'c1_suspended' => '2 suspended',
             ],
